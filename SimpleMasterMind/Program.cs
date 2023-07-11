4c79742faf05338b19633852463bd0bc179b9f6f
Cange Jérémy <github@cange-jeremy.be>
2023-07-11 10:14:55 +0200

    Add project files.

---
+++ SimpleMasterMind/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleMasterMind
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
